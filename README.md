bba2a6fe6aea37aa83b160014230ca8a88a94a08
Emily Spradling <spradling.ed@gmail.com>
2023-03-11 10:07:40 -0600

    Initial English game quiz

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # python-games-2
+learning exercises adapted from https://www.youtube.com/watch?v=DLn3jOsNRVE
---
+++ quiz.py
@@ -0,0 +1,40 @@
+print("Welcome to the present perfect English quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay, let's play! Fill in the blanks below with the correct answer in the present perfect.")
+score = 0
+
+answer = input("I _____ never ______ (be) to space. ")
+if answer.lower() == "have been":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("_____ you ever ______ (fall) in love? ")
+if answer.lower() == "have fallen":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("She _____ _______ (not quit) a job before. ")
+if answer.lower() == "hasn't quit":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("How many movies _______ he _________ (see) today? ")
+if answer.lower() == "has seen":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+print("You got " + str(score) + " questions correct!")
+print("You got " + str((score / 4) * 100) + "%.")
