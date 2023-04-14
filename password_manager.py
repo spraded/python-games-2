b49602d0d549c8389fa688c90a136237c83361af
Emily Spradling <spradling.ed@gmail.com>
2023-04-14 09:30:16 -0500

    Mock password manager basic setup

---
+++ password_manager.py
@@ -0,0 +1,28 @@
+master_pwd = input("Type your master password. ")
+
+def view():
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User:", user, "| Password:", passw)
+
+def add():
+    name = input('Account Name: ')
+    pwd = input("Password: ")
+
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + pwd + "\n")
+
+while True:
+    mode = input("Would you like to add a new password or view existing? Type view, add or q to quit. ").lower()
+    if mode == "q":
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid option. Type add or view.")
+        continue
